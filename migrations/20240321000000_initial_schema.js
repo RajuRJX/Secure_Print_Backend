7b1fa2f4576ca3460766307d18e02d4e75da5da1
RajuRJX <your-github-email@example.com>
2025-05-27 20:36:36 +0530

    updated database script to include encryption-key

---
+++ migrations/20240321000000_initial_schema.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('users', function(table) {
+        table.increments('id').primary();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable();
+        table.timestamps(true, true);
+      })
+      .createTable('documents', function(table) {
+        table.increments('id').primary();
+        table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
+        table.string('file_name').notNullable();
+        table.string('file_path').notNullable();
+        table.string('file_type').notNullable();
+        table.binary('encryption_key').notNullable();
+        table.timestamps(true, true);
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('documents')
+      .dropTableIfExists('users');
+  };
